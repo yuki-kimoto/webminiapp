c941a95e0b4cbcc81e2b834cc56af39e1ba62754
Yuki Kimoto <kimoto.yuki@gmail.com>
2012-06-10 20:49:51 +0900

    Added application files

---
+++ lib/Webminiapp.pm
@@ -0,0 +1,20 @@
+package Webminiapp;
+use Mojo::Base 'Mojolicious';
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+  
+  $self->plugin('Config');
+
+  # Documentation browser under "/perldoc"
+  $self->plugin('PODRenderer');
+
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->get('/')->to('example#welcome');
+}
+
+1;
